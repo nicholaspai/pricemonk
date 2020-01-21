a3f794c1a2b58acba069e25e8eeadbb75de99371
Nick Pai <npai.nyc@gmail.com>
2020-01-21 16:48:09 -0500

    WIP: slack bot
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- server.js
+++ server.js
@@ -23,6 +23,22 @@ app.get('/price', async function(req, res) {
         res.status(400).send('Invalid "currency" and "date" parameters');
     }
 })
+app.post('/price/slack', async function (req, res) {
+    if (true) {
+        try {
+            let text = req.body.text.split(' ');
+            console.log('received text:', text);
+            // let currency = req.query.currency.toUpperCase();
+            // let date = req.query.date;        
+            // let price = await getPrice(currency, date);
+            // res.json(price);        
+        } catch (err) {
+            res.status(500).send('Server failed to load a price');
+        }
+    } else {
+        res.status(400).send('Invalid "currency" and "date" parameters');
+    }
+})
 
 // Start server
 app.listen(PORT);
